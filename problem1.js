58338f8e023a7c759d5f7d0d3e074f1270eb300a
despairMoves <nilotpalbezbaruah1859@gmail.com>
2023-04-10 20:09:30 +0530

    To Know Whether The Number is Odd or Even

---
+++ problem1.js
@@ -0,0 +1,5 @@
+// Source code to find whether a number is odd or even. Written by Nilutpal Bezbaruah, 10/4/2023
+
+let number = 45689;
+
+(number%2 == 0)?console.log("number is even"):console.log("number is odd");
